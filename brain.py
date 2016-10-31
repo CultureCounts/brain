8206b80f8b0be5f20248106e2d3873bc43d63e99
Chris McCormick <chris@mccormick.cx>
2016-10-31 14:52:00 +0800

    Functional programming style.

--- brain.py
+++ brain.py
@@ -1,29 +1,29 @@
+import os
+import imp
 import json
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
 from pprint import pprint
 
-class S(BaseHTTPRequestHandler):
-    def do_POST(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        data = json.loads(self.rfile.read(int(self.headers['Content-Length'])))
-        for d in data:
-            print d.get("time"), d.get("host"), d.get("plugin"), d.get("type"), d.get("type_instance"), d.get("plugin_instance"), d.get("dsnames"), d.get("values")
-            #pprint(d)
-        self.wfile.write("true")
+def handle_post(handler):
+    handler.send_response(200)
+    handler.send_header('Content-type', 'text/plain')
+    handler.end_headers()
+    data = json.loads(handler.rfile.read(int(handler.headers['Content-Length'])))
+    for d in data:
+        print d.get("time"), d.get("host"), d.get("plugin"), d.get("type"), d.get("type_instance"), d.get("plugin_instance"), d.get("dsnames"), d.get("values")
+        #pprint(d)
+    handler.wfile.write("true")
 
-def run(server_class=HTTPServer, handler_class=S, port=8555):
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
+class handler(BaseHTTPRequestHandler):
+    do_POST = handle_post
+
+def run_server():
+    httpd = HTTPServer(('', 8555), handler)
     print 'Starting brain httpd server...'
     httpd.serve_forever()
 
 if __name__ == "__main__":
     from sys import argv
+    run_server()
 
-if len(argv) == 2:
-    run(port=int(argv[1]))
-else:
-    run()
