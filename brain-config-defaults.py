70bb68de2f87e7b677aedb5f28b8630b675d3eb3
Chris McCormick <chris@mccormick.cx>
2016-11-01 14:33:08 +0800

    Config example with no dependencies.

--- brain-config-defaults.py
+++ brain-config-defaults.py
@@ -1,10 +1,7 @@
-from functools import partial
-from checks import check_exceed
-
 MATCHES = [
     {"plugin": "df", "type": "df_complex", "type_instance": "free", "plugin_instance": "root"},
     {"plugin": "memory", "type_instance": "free"},
-    {"plugin": "load", "check": partial(check_exceed, 0.1)},
+    {"plugin": "load", "check": lambda d: d.get("values", [0, 0, 0])[0] > 0.1 and "CPU load is high (%.2f)" % d.get("values")[0]},
     {"plugin": "swap", "type_instance": "free"},
 ]
 
