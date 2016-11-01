7e5827ddb154d76cb03a6a0e239b052307e47bd0
Chris McCormick <chris@mccormick.cx>
2016-11-01 09:31:28 +0800

    Check for only matching updates from collected.

--- brain.py
+++ brain.py
@@ -5,14 +5,19 @@ from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
 from pprint import pprint
 
+def check_match(matchers, entry):
+    for m in matchers:
+        if all(item in entry.items() for item in m.items()):
+            return True
+
 def handle_post(handler):
     handler.send_response(200)
     handler.send_header('Content-type', 'text/plain')
     handler.end_headers()
     data = json.loads(handler.rfile.read(int(handler.headers['Content-Length'])))
     for d in data:
-        print d.get("time"), d.get("host"), d.get("plugin"), d.get("type"), d.get("type_instance"), d.get("plugin_instance"), d.get("dsnames"), d.get("values")
-        #pprint(d)
+        if check_match(handler.config.MATCHES, d):
+            print "MATCH", d.get("time"), d.get("host"), d.get("plugin"), d.get("type"), d.get("type_instance"), d.get("plugin_instance"), d.get("dsnames"), d.get("values")
     handler.wfile.write("true")
 
 def run_server(config):
