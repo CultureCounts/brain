41709d2842a76a7b4006997236faa55cdbf207ed
Chris McCormick <chris@mccormick.cx>
2016-11-01 09:31:40 +0800

    Forgot default config.

---
+++ brain-config-defaults.py
@@ -0,0 +1,8 @@
+MATCHES = [
+    {"plugin": "df", "type": "df_complex", "type_instance": "free", "plugin_instance": "root"},
+    {"plugin": "memory", "type_instance": "free"},
+    {"plugin": "load"},
+    {"plugin": "swap", "type_instance": "free"},
+]
+
+
